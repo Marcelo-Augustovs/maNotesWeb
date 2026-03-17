52727e11add05ae7f64557db92ada3d8fb3e37a2
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2026-03-17 08:53:36 -0300

    mapped some routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { DashBordComponent } from './features/dash-bord/dash-bord.component';
+import { NotesComponent } from './features/notes/notes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: DashBordComponent
+  },
+
+  {
+    path: 'dashboard',
+    component: DashBordComponent
+  },
+
+  {
+    path: 'notes',
+    component: NotesComponent
+  }
+
+];
