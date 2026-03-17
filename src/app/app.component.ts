21f4fcbd2377ba7ae40ef8f4f9b71771b2b02c76
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2026-03-17 08:38:41 -0300

    create main menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from '../shared/components/side-menu/side-menu.component';
+import { TopBarComponent } from '../shared/components/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SideMenuComponent,TopBarComponent],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'MaNotesWeb';
