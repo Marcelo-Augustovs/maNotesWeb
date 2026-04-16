8e88ef3ee2299afea461c3d49f548e56eaa964ef
Marcelo Augusto <marcelo.augustovs99@gmail.com>
2026-04-16 11:07:40 -0300

    fix: Otimizacao da estrutura global

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { SideMenuComponent } from '../shared/components/side-menu/side-menu.component';
 import { TopBarComponent } from '../shared/components/top-bar/top-bar.component';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SideMenuComponent,TopBarComponent],
+  imports: [RouterOutlet, SideMenuComponent, TopBarComponent, CommonModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MaNotesWeb';
+  isLoginPage = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.isLoginPage = event.urlAfterRedirects.includes('/login');
+    });
+  }
 }
